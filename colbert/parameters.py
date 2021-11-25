fef8c89a11b2ec2c30e09e8c2949f8e280c37ab2
Trang Dieu <trangdieu.kp@gmail.com>
2021-11-25 14:53:16 -0500

    Add inputs for index.py, index_faiss.py and retrieve.py

--- colbert/parameters.py
+++ colbert/parameters.py
@@ -1,30 +1,54 @@
 import torch
 
+# Device
 DEVICE = torch.device("cuda")
 
+# Saved checkpoints
 A = 100000
 SAVED_CHECKPOINTS = [A, A * 2, A * 3, A * 4, A * 5, A * 6, A * 7, A * 8, A * 9, A * 10]
-SAVED_CHECKPOINTS += [
-    A * 11,
-    A * 12,
-    A * 13,
-    A * 14,
-    A * 15,
-    A * 16,
-    A * 17,
-    A * 18,
-    A * 19,
-    A * 20,
-]
-SAVED_CHECKPOINTS += [
-    A * 21,
-    A * 22,
-    A * 23,
-    A * 24,
-    A * 25,
-    A * 26,
-    A * 27,
-    A * 28,
-    A * 29,
-    A * 30,
-]
+
+
+# Parameters for train.py
+def train_params():
+    return [
+        "amp",
+        # Uncomment below when necessary
+        # "resume",
+        # "resume_optimizer",
+        # "checkpoint", ""
+        # "triples", "",
+        # "queries", "",
+        # "collection", ""
+    ]
+
+
+# Parameters for index.py
+def index_params():
+    return [
+        "amp",
+        "checkpoint",
+        "colbert/model_checkpoint/biobert-MM-2970159.pt",
+        "index_name",
+        "BioSciNF.L2.8x2970159",
+    ]
+
+
+# Parameters for index_faiss.py
+def index_faiss_params():
+    return ["index_name", "BioSciNF.L2.8x2970159", "slices", 39]
+
+
+# Parameters for retrieve.py
+def retrieve_params():
+    return [
+        "amp",
+        "checkpoint",
+        "colbert/model_checkpoint/biobert-MM-2970159.pt",
+        "index_name",
+        "BioSciNF.L2.8x2970159",
+        "slices",
+        39,
+        "num_faiss_indexes",
+        39,
+        # "queries", "", # Uncomment if necessary
+    ]
