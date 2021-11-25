65b8634d3a1c6db204cc2165ad9d56a078d21630
Trang Dieu <trangdieu.kp@gmail.com>
2021-11-25 16:07:07 -0500

    Delete schemas

--- ETL/article.py
+++ ETL/article.py
@@ -8,25 +8,6 @@ class Article:
     Article object. Holds metadata, abstract and title of that article.
     """
 
-    # Article schema
-    # ARTICLE = (
-    #    "article_id",
-    #    "source",
-    #    "title",
-    #    "abtract",
-    #    "published_date",
-    #    "authors",
-    #    "journal",
-    #    "url",
-    #    "entry_date",
-    # )
-
-    # Sections schema
-    # SECTIONS = ("span_id", "text")
-
-    # Sentences schema
-    # SENTENCES = ("article_id", "sentence_id", "sentence")
-
     def __init__(self, metadata, sections, sentences):
         """
         Stores article metadata and content as an object.
